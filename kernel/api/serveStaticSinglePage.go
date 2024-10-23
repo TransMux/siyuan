4505be21dd5eddcfee6956ff8be5abf95ff16de4
Muxuan Li <InEase28@gmail.com>
2024-10-24 01:55:02 +0800

    ✅ 返回html

--- kernel/api/serveStaticSinglePage.go
+++ kernel/api/serveStaticSinglePage.go
@@ -1,19 +1,3 @@
-// SiYuan - Refactor your thinking
-// Copyright (c) 2020-present, b3log.org
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 package api
 
 import (
@@ -25,19 +9,17 @@ import (
 )
 
 func serveStaticSinglePage(c *gin.Context) {
-	// func getDoc(c *gin.Context) {
 	ret := gulu.Ret.NewResult()
-	defer c.JSON(http.StatusOK, ret)
 
 	id := c.Param("block_id")
 	if "" == id {
 		ret.Code = -1
 		ret.Msg = "block ID is required"
+		c.JSON(http.StatusOK, ret)
 		return
 	}
 
 	index := 0
-
 	var query string
 	var queryMethod int
 	var queryTypes map[string]bool
@@ -47,31 +29,20 @@ func serveStaticSinglePage(c *gin.Context) {
 	endID := ""
 	isBacklink := false
 
-	blockCount, content, parentID, parent2ID, rootID, typ, eof, scroll, boxID, docPath, isBacklinkExpand, err := model.GetDoc(startID, endID, id, index, query, queryTypes, queryMethod, mode, size, isBacklink)
+	_, content, _, _, _, _, _, _, _, _, _, err := model.GetDoc(startID, endID, id, index, query, queryTypes, queryMethod, mode, size, isBacklink)
 	if model.ErrBlockNotFound == err {
 		ret.Code = 3
+		c.JSON(http.StatusOK, ret)
 		return
 	}
 
 	if err != nil {
 		ret.Code = 1
 		ret.Msg = err.Error()
+		c.JSON(http.StatusOK, ret)
 		return
 	}
 
-	ret.Data = map[string]interface{}{
-		"id":               id,
-		"mode":             mode,
-		"parentID":         parentID,
-		"parent2ID":        parent2ID,
-		"rootID":           rootID,
-		"type":             typ,
-		"content":          content,
-		"blockCount":       blockCount,
-		"eof":              eof,
-		"scroll":           scroll,
-		"box":              boxID,
-		"path":             docPath,
-		"isBacklinkExpand": isBacklinkExpand,
-	}
+	c.Header("Content-Type", "text/html; charset=utf-8")
+	c.String(http.StatusOK, content)
 }
