2c6fb2ff7ee75c54139363ed6e55de78e1eab980
Muxuan Li <InEase28@gmail.com>
2025-01-04 16:54:13 +0800

    ✅ 支持外部输入移动

--- app/src/mux/attributeView.ts
+++ app/src/mux/attributeView.ts
@@ -10,6 +10,9 @@ const 关系笔记目录 = "20250101010038-81wd8r8";
 const 标签之树avID = "20250101001630-x03k4te";
 const 标签之树目录 = "20250101005307-7384qo5";
 
+const 外部输入avID = "20250102171020-4cqqonx";
+const 外部输入目录 = "20250102171013-zzcyz96";
+
 export async function afterAddedFileToDatabase(file_ids: Array<string>, avID: string) {
     console.log("添加文档到数据库callback: ", file_ids, avID);
 
@@ -50,6 +53,13 @@ export async function afterAddedFileToDatabase(file_ids: Array<string>, avID: st
         fetchPost("/api/filetree/moveDocsByID", {
             "fromIDs": file_ids,
             "toID": 标签之树目录
+        });
+    }
+
+    if (avID === 外部输入avID) {
+        fetchPost("/api/filetree/moveDocsByID", {
+            "fromIDs": file_ids,
+            "toID": 外部输入目录
         });
     }
 }
