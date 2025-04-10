1de4b8bce1905a0735b9f2b79349de2d0738c751
Muxuan Li <InEase28@gmail.com>
2025-04-10 22:37:29 +0800

    unlock

--- kernel/model/conf.go
+++ kernel/model/conf.go
@@ -876,22 +876,23 @@ func InitBoxes() {
 }
 
 func IsSubscriber() bool {
-	u := Conf.GetUser()
-	return nil != u && (-1 == u.UserSiYuanProExpireTime || 0 < u.UserSiYuanProExpireTime) && 0 == u.UserSiYuanSubscriptionStatus
+	return true
+	// u := Conf.GetUser()
+	// return nil != u && (-1 == u.UserSiYuanProExpireTime || 0 < u.UserSiYuanProExpireTime) && 0 == u.UserSiYuanSubscriptionStatus
 }
 
 func IsPaidUser() bool {
 	// S3/WebDAV data sync and backup are available for a fee https://github.com/siyuan-note/siyuan/issues/8780
+	return true
+	// if IsSubscriber() {
+	// 	return true
+	// }
 
-	if IsSubscriber() {
-		return true
-	}
-
-	u := Conf.GetUser()
-	if nil == u {
-		return false
-	}
-	return 1 == u.UserSiYuanOneTimePayStatus
+	// u := Conf.GetUser()
+	// if nil == u {
+	// 	return false
+	// }
+	// return 1 == u.UserSiYuanOneTimePayStatus
 }
 
 const (
