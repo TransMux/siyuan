a4790d0a968ba1779a0294fef0d62999ac0286b5
Muxuan Li <InEase28@gmail.com>
2025-07-18 13:30:18 +0800

    移除

--- app/src/plugin/builtin/siyuan-auto-seq-number/lib/utils/dom_operate.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function setCursorToEnd(element: any): void;
--- app/src/plugin/builtin/siyuan-auto-seq-number/lib/utils/seq_num.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function getSeqNumber(level: number, counters: number[], seqNumConfigs: string[], num2ChiConfigs: boolean[]): [string, number[]];
-export declare function num2Chi(numStr: string): string;
-export declare function isHeaderText(text: string | null | undefined): number;
